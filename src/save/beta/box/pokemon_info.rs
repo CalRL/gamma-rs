a801cc33ed17d072304a104036f9234a134de6d4
Cal <callumburnsoregan@gmail.com>
2026-08-06 01:03:19 +0200

    Update get_name and get_name_mut

--- src/save/beta/box/pokemon_info.rs
+++ src/save/beta/box/pokemon_info.rs
@@ -1,10 +1,11 @@
 use crate::save::beta::pokemon::stats::{StatStruct, Stats};
 use crate::traits::{NamespacedValue, StartsWith};
-use crate::utils::custom_struct::{get_struct_at_idx_mut, get_struct_property_at_idx};
+use crate::utils::custom_struct::{get_struct_property_at_idx};
 use gvas::GvasFile;
+use gvas::properties::array_property::ArrayProperty;
 use gvas::properties::Property;
 use gvas::properties::int_property::BytePropertyValue;
-use gvas::properties::struct_property::StructProperty;
+use gvas::properties::struct_property::{StructProperty, StructPropertyValue};
 use gvas::properties::text_property::FTextHistory;
 use std::collections::HashMap;
 
@@ -108,41 +109,30 @@ pub fn get_level(properties: &StructProperty) -> Option<i32> {
     }
 }
 
-pub fn get_name(properties: &StructProperty) -> Option<&String> {
-    let vec = properties.get_starts_with("Name")?;
-    let name_prop = vec.first()?;
-
-    let history = match name_prop {
-        Property::TextProperty(text) => Some(&text.value.history),
-        _ => None,
+pub fn get_name(property: &StructPropertyValue) -> Option<&String> {
+    let prop = get_first(property)?;
+    let text = match prop {
+        Property::TextProperty(text) => Some(text),
+        _ => None
     }?;
 
-    let source_string = match history {
-        FTextHistory::Base { source_string, .. } => match source_string {
-            None => None,
-            Some(str) => Some(str),
-        },
-        _ => return None,
-    };
-
-    source_string
+    match &text.value.history {
+        FTextHistory::Base { source_string: name, .. } => Some(name),
+        _ => None
+    }?.as_ref()
 }
 
-pub fn get_name_mut(properties: &mut StructProperty) -> Option<&mut String> {
-    let vec = properties.get_starts_with_mut("Name")?;
-    let name_prop = vec.first_mut()?;
+pub fn get_name_mut(property: &mut Property) -> Option<&mut String> {
+    let prop = get_first_mut(property)?;
+    let text = match prop {
+        Property::TextProperty(text) => Some(text),
+        _ => None
+    }?;
 
-    if let Property::TextProperty(text) = name_prop {
-        if let FTextHistory::Base {
-            source_string: Some(s),
-            ..
-        } = &mut text.value.history
-        {
-            return Some(s);
-        }
-    }
-
-    None
+    match &mut text.value.history {
+        FTextHistory::Base { source_string: name, .. } => Some(name),
+        _ => None
+    }?.as_mut()
 }
 
 /// Returns a namespaced string
@@ -179,6 +169,32 @@ pub struct PokemonInfo<'a> {
     property: &'a Property,
 }
 
+fn get_first(property: &StructPropertyValue) -> Option<&Property> {
+    property
+        .get_custom_struct()?
+        .0
+        .iter()
+        .find(|(key, _)| key.starts_with("Name"))?
+        .1
+        .first()
+}
+
+fn get_first_mut(property: &mut Property) -> Option<&mut Property> {
+    let arr = match property {
+        Property::ArrayProperty(arr) => arr,
+        _ => return None
+    };
+    match arr {
+        ArrayProperty::Structs { structs, .. } => Some(structs),
+        _ => None
+    }?
+        .first_mut()?
+        .get_custom_struct_mut()?
+        .0
+        .get_mut("Name")?
+        .first_mut()
+}
+
 impl<'a> PokemonInfo<'a> {
     /// Todo: turn this into a trait
     pub fn new_party(gvas_file: &'a GvasFile) -> Option<Self> {
@@ -192,53 +208,112 @@ impl<'a> PokemonInfo<'a> {
         get_name(struct_at)
     }
 
-    pub fn get_nature(&self, index: usize) -> Option<String> {
-        let struct_at = get_struct_property_at_idx(self.property, index)?;
-        get_nature_string(struct_at).cloned()
-    }
-
-    pub fn get_primary_type(&self, index: usize) -> Option<String> {
-        let struct_at = get_struct_property_at_idx(self.property, index)?;
-        get_primary_type_string(struct_at).cloned()
-    }
-
-    pub fn get_secondary_type(&self, index: usize) -> Option<String> {
-        let struct_at = get_struct_property_at_idx(self.property, index)?;
-        get_secondary_type_string(struct_at).cloned()
-    }
-    pub fn get_stats(&self, index: usize) -> Option<StatStruct> {
-        let struct_at = get_struct_property_at_idx(self.property, index)?;
-        get_stats(struct_at)
-    }
-    pub fn get_stat(&self, index: usize, stat: Stats) -> Option<f64> {
-        let struct_at = get_struct_property_at_idx(self.property, index)?;
-        get_stat(struct_at, stat)
-    }
+    // pub fn get_nature(&self, index: usize) -> Option<String> {
+    //     let struct_at = get_struct_property_at_idx(self.property, index)?;
+    //     get_nature_string(struct_at).cloned()
+    // }
+    //
+    // pub fn get_primary_type(&self, index: usize) -> Option<String> {
+    //     let struct_at = get_struct_property_at_idx(self.property, index)?;
+    //     get_primary_type_string(struct_at).cloned()
+    // }
+    //
+    // pub fn get_secondary_type(&self, index: usize) -> Option<String> {
+    //     let struct_at = get_struct_property_at_idx(self.property, index)?;
+    //     get_secondary_type_string(struct_at).cloned()
+    // }
+    // pub fn get_stats(&self, index: usize) -> Option<StatStruct> {
+    //     let struct_at = get_struct_property_at_idx(self.property, index)?;
+    //     get_stats(struct_at)
+    // }
+    // pub fn get_stat(&self, index: usize, stat: Stats) -> Option<f64> {
+    //     let struct_at = get_struct_property_at_idx(self.property, index)?;
+    //     get_stat(struct_at, stat)
+    // }
 }
 
 pub struct PokemonInfoMut<'a> {
     property: &'a mut Property,
 }
 
-impl<'a> PokemonInfoMut<'a> {
-    pub fn new_party(gvas_file: &'a mut GvasFile) -> Option<Self> {
-        let prop = gvas_file.properties.get_mut("PartyPokemonInfo")?;
-        Some(Self { property: prop })
+// impl<'a> PokemonInfoMut<'a> {
+//     pub fn new_party(gvas_file: &'a mut GvasFile) -> Option<Self> {
+//         let prop = gvas_file.properties.get_mut("PartyPokemonInfo")?;
+//         Some(Self { property: prop })
+//     }
+//
+//     pub fn set_stat(&mut self, index: usize, stat: Stats, value: f64) {
+//         if let Some(s) = get_struct_at_idx_mut(self.property, index) {
+//             if let Some(stat_ref) = get_stat_mut(s, stat) {
+//                 *stat_ref = value;
+//             }
+//         }
+//     }
+//
+//     pub fn set_name(&mut self, index: usize, name: String) {
+//         if let Some(s) = get_struct_at_idx_mut(self.property, index) {
+//             if let Some(name_ref) = get_name_mut(s) {
+//                 *name_ref = name;
+//             }
+//         }
+//     }
+// }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use gvas::game_version::GameVersion;
+    use std::fs::File;
+
+    const SLOT1_PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/saves/Slot1.sav");
+
+    struct NameCase<'a> {
+        array_name: &'a str,
+        idx: usize,
+        expected_name: &'a str,
     }
 
-    pub fn set_stat(&mut self, index: usize, stat: Stats, value: f64) {
-        if let Some(s) = get_struct_at_idx_mut(self.property, index) {
-            if let Some(stat_ref) = get_stat_mut(s, stat) {
-                *stat_ref = value;
-            }
+    fn generate_case<'a>(array_name: &'a str, idx: usize, expected_name: &'a str) -> NameCase<'a> {
+        NameCase {
+            array_name,
+            idx,
+            expected_name,
         }
     }
 
-    pub fn set_name(&mut self, index: usize, name: String) {
-        if let Some(s) = get_struct_at_idx_mut(self.property, index) {
-            if let Some(name_ref) = get_name_mut(s) {
-                *name_ref = name;
-            }
+    fn load_slot1() -> GvasFile {
+        let mut file = File::open(SLOT1_PATH).expect("save file exists");
+        GvasFile::read(&mut file, GameVersion::Default).expect("gvas file reads")
+    }
+
+    fn assert_name(gvas_file: &GvasFile, case: &NameCase<'_>) {
+        let property = gvas_file
+            .properties
+            .get(case.array_name)
+            .expect("array property exists");
+        let pokemon_info = get_struct_property_at_idx(property, case.idx).expect("pokemon info exists");
+        let name = get_name(pokemon_info).expect("pokemon name exists");
+
+        assert_eq!(
+            name, case.expected_name,
+            "name mismatch for {}[{}]",
+            case.array_name, case.idx
+        );
+    }
+
+    #[test]
+    fn reads_names_from_gvas_file() {
+        let gvas_file = load_slot1();
+
+        let cases = &[
+            generate_case("Box1PokemonInfo", 1, "SALAMENCE"),
+            generate_case("Box1PokemonInfo", 0, "NAME"),
+            generate_case("PartyPokemonInfo", 0, "METAGROSS"),
+        ];
+
+        assert!(!cases.is_empty(), "add pokemon info name test cases");
+        for case in cases {
+            assert_name(&gvas_file, case);
         }
     }
 }
