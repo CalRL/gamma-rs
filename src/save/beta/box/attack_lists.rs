4ad047ec2cd5f2610c16be0e57a657cf59dbd0bc
Cal <callumburnsoregan@gmail.com>
2026-08-06 00:45:24 +0200

    Update attack_lists.rs
    to 0.11.0

--- src/save/beta/box/attack_lists.rs
+++ src/save/beta/box/attack_lists.rs
@@ -1,6 +1,6 @@
 use gvas::properties::Property;
 use gvas::properties::array_property::ArrayProperty;
-use gvas::properties::struct_property::StructProperty;
+use gvas::properties::struct_property::StructPropertyValue;
 
 // Takes the "PartyAttackLists" property
 pub fn attack_array(property: &Property) -> Option<&ArrayProperty> {
@@ -12,14 +12,15 @@ pub fn attack_array(property: &Property) -> Option<&ArrayProperty> {
     Some(cs)
 }
 /// Returns the custom struct
+// TODO: test this
 pub fn attacks_at(array: &ArrayProperty, index: usize) -> Option<&ArrayProperty> {
-    let property: &StructProperty = match &array {
+    let property: &StructPropertyValue = match &array {
         ArrayProperty::Structs { structs, .. } => structs.get(index)?,
         _ => return None,
     };
 
-    let (_, v) = property.value.get_custom_struct()?;
-    for (key, val) in v.iter() {
+    let custom_struct = property.get_custom_struct()?;
+    for (key, val) in custom_struct.iter() {
         if key.starts_with("Attacks_") {
             let first = val.first()?;
             return match &first {
@@ -28,7 +29,6 @@ pub fn attacks_at(array: &ArrayProperty, index: usize) -> Option<&ArrayProperty>
             };
         }
     }
-
     None
 }
 
@@ -56,3 +56,71 @@ pub fn parse_attack(attack: &str) -> Option<String> {
 }
 
 pub struct AttackLists;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use gvas::GvasFile;
+    use gvas::game_version::GameVersion;
+    use std::fs::File;
+
+    const SLOT1_PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/saves/Slot1.sav");
+
+    struct AttackCase<'a> {
+        array_property_name: &'a str,
+        pokemon_index: usize,
+        move_idx: usize,
+        expected_attack: &'a str,
+    }
+
+    fn generate_case<'a>(
+        array_property_name: &'a str,
+        pokemon_index: usize,
+        move_idx: usize,
+        expected_attack: &'a str,
+    ) -> AttackCase<'a> {
+        AttackCase {
+            array_property_name,
+            pokemon_index,
+            move_idx,
+            expected_attack,
+        }
+    }
+
+    fn load_slot1() -> GvasFile {
+        let mut file = File::open(SLOT1_PATH).expect("save file exists");
+        GvasFile::read(&mut file, GameVersion::Default).expect("gvas file reads")
+    }
+
+    fn assert_attack_value(gvas_file: &GvasFile, case: &AttackCase<'_>) {
+        let property = gvas_file
+            .properties
+            .get(case.array_property_name)
+            .expect("array property exists");
+        let array = attack_array(property).expect("property is an array");
+        let attacks = attacks_at(array, case.pokemon_index).expect("pokemon has an attacks array");
+        let attack = attack_at(attacks, case.move_idx).expect("move has an attack value");
+        let parsed_attack = parse_attack(attack).expect("attack value parses");
+
+        assert_eq!(
+            parsed_attack, case.expected_attack,
+            "attack mismatch for {}[{}][{}]",
+            case.array_property_name, case.pokemon_index, case.move_idx
+        );
+    }
+
+    #[test]
+    fn reads_attack_lists_from_gvas_file() {
+        let gvas_file = load_slot1();
+
+        let cases = &[
+            generate_case("Box1AttackLists", 1, 0, "Growl"),
+            generate_case("PartyAttackLists", 0, 0, "Astonish"),
+        ];
+
+        assert!(!cases.is_empty(), "add attack list test cases");
+        for case in cases {
+            assert_attack_value(&gvas_file, case);
+        }
+    }
+}
