84c2af73b33f7471fd94de694f225b34674afc1c
Cal <callumburnsoregan@gmail.com>
2026-08-08 03:34:25 +0200

    add: impl TryFrom<BetaEnumStr> for Natures

--- src/save/beta/pokemon/natures.rs
+++ src/save/beta/pokemon/natures.rs
@@ -1,4 +1,29 @@
 use crate::save::beta::BetaEnumStr;
+use crate::utils::get_enum_number;
+
+// ENUM_Natures::NewEnumerator0 hardy
+// 1 lonely
+// 2 brave
+// 3 adamant
+// ENUM_Natures::NewEnumerator6 docile
+// ENUM_Natures::NewEnumerator7 relaxed
+// 8 impish
+// 9 lax
+// ENUM_Natures::NewEnumerator10 timid
+// 11 hasty
+// ENUM_Natures::NewEnumerator12 serious
+// 13 jolly
+// 14 naive
+// 15 modest
+// ENUM_Natures::NewEnumerator16 mild
+// ENUM_Natures::NewEnumerator17 quiet
+// 18 bashful
+// 19 rash
+// 20 calm
+// 21 gentle
+// 22 sassy
+// 23 careful
+// ENUM_Natures::NewEnumerator24 quirky
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Nature {
@@ -57,8 +82,35 @@ impl From<Nature> for i32 {
     }
 }
 
-impl From<BetaEnumStr<'_>> for Nature {
-    fn from(value: BetaEnumStr) -> Self {
-        todo!()
+impl TryFrom<BetaEnumStr<'_>> for Nature {
+    type Error = ();
+
+    fn try_from(value: BetaEnumStr<'_>) -> Result<Self, Self::Error> {
+        match get_enum_number(value.0).ok_or(())? {
+            0 => Ok(Nature::Hardy),
+            1 => Ok(Nature::Lonely),
+            2 => Ok(Nature::Brave),
+            3 => Ok(Nature::Adamant),
+            6 => Ok(Nature::Docile),
+            7 => Ok(Nature::Relaxed),
+            8 => Ok(Nature::Impish),
+            9 => Ok(Nature::Lax),
+            10 => Ok(Nature::Timid),
+            11 => Ok(Nature::Hasty),
+            12 => Ok(Nature::Serious),
+            13 => Ok(Nature::Jolly),
+            14 => Ok(Nature::Naive),
+            15 => Ok(Nature::Modest),
+            16 => Ok(Nature::Mild),
+            17 => Ok(Nature::Quiet),
+            18 => Ok(Nature::Bashful),
+            19 => Ok(Nature::Rash),
+            20 => Ok(Nature::Calm),
+            21 => Ok(Nature::Gentle),
+            22 => Ok(Nature::Sassy),
+            23 => Ok(Nature::Careful),
+            24 => Ok(Nature::Quirky),
+            _ => Err(()),
+        }
     }
 }
