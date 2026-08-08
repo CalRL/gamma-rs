47d3be40b3b25f19dbd4e373507d3c6d4ac93acc
Cal <callumburnsoregan@gmail.com>
2026-08-08 03:47:33 +0200

    Update mod.rs

--- tests/beta/boxes/mod.rs
+++ tests/beta/boxes/mod.rs
@@ -1,9 +1,11 @@
 pub mod attack_lists;
 pub mod caught_ball;
 pub mod iv_struct;
+pub mod natures;
 pub mod pokemon_classes;
 pub mod pokemon_gender;
 pub mod pokemon_id;
 pub mod pokemon_info;
 pub mod pp_moves_lists;
 pub mod shiny_list;
+pub mod row_slot_id;
